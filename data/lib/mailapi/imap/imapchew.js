35d4fe75ec6967c133682beba7395d3c49e197a9
Alex Wissmann <awiss@mozilla.com>
2014-06-03 14:12:24 -0700

    Bug 851963 - Fixed issue where ios shared images would not appear as attachments

--- data/lib/mailapi/imap/imapchew.js
+++ data/lib/mailapi/imap/imapchew.js
@@ -135,19 +135,31 @@ function chewStructure(msg) {
       filename = null;
     }
 
-    // - Start from explicit disposition, make attachment if non-displayable
-    if (partInfo.disposition)
-      disposition = partInfo.disposition.type.toLowerCase();
-    // UNTUNED-HEURISTIC (need test cases)
-    // Parts with content ID's explicitly want to be referenced by the message
-    // and so are inline.  (Although we might do well to check if they actually
-    // are referenced.  This heuristic could be very wrong.)
-    else if (partInfo.id)
-      disposition = 'inline';
-    else if (filename || partInfo.type !== 'text')
+    // Determining disposition:
+
+    // First, check whether an explict one exists
+    if (partInfo.disposition) {
+      // If it exists, keep it the same, except in the case of inline
+      // disposition without a content id. 
+      if (partInfo.disposition.type.toLowerCase() == 'inline'){
+        if(partInfo.id) {
+          disposition = 'inline';
+        } else {
+          disposition = 'attachment';
+        }
+      } else if(partInfo.disposition.type.toLowerCase() == 'attachment')  {
+        disposition = 'attachment';
+      // This case should never trigger, but it's here for safety's sake
+      } else {
+        disposition = 'inline';
+      }
+    // If there is no explicit disposition, all non-text parts or ones with a 
+    // filename are made attachments.
+    } else if (filename || partInfo.type !== 'text') {
       disposition = 'attachment';
-    else
+    } else {
       disposition = 'inline';
+    }
 
     // Some clients want us to display things inline that we simply can't
     // display (historically and currently, PDF) or that our usage profile
